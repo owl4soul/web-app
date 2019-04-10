0d137de4c8b0e349e362f6ac6ec9c2f3aafc7e86
owl4soul <resuuser989@gmail.com>
2019-04-11 00:24:57 +0300

    hibernate.cfg.xml

--- src/main/java/com/github/owl4soul/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/com/github/owl4soul/utils/HibernateSessionFactoryUtil.java
@@ -16,7 +16,7 @@ public class HibernateSessionFactoryUtil {
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
             try {
-                Configuration configuration = new Configuration().configure();
+                Configuration configuration = new Configuration().configure("/resources/hibernate.cfg.xml");
                 configuration.addAnnotatedClass(User.class);
                 StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
                 sessionFactory = configuration.buildSessionFactory(builder.build());
